0c10608d6f0fe82d21a1ec54e4a39e9b704ab4bc
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-09-28 11:45:03 -0300

    Finalização do projeto

---
+++ desafioFinalModulo1/app/src/main/java/com/example/desafiofinalmodulo1/Accounts.java
@@ -0,0 +1,52 @@
+package com.example.desafiofinalmodulo1;
+
+public class Accounts {
+    private int id;
+    private int agencia;
+    private int conta;
+    private String name;
+    private int balance;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public void setAgencia(int agencia) {
+        this.agencia = agencia;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public void setConta(int conta) {
+        this.conta = conta;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+
+
+}
--- desafioFinalModulo1/app/src/main/java/com/example/desafiofinalmodulo1/MainActivity.java
+++ desafioFinalModulo1/app/src/main/java/com/example/desafiofinalmodulo1/MainActivity.java
@@ -2,13 +2,85 @@ package com.example.desafiofinalmodulo1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.JsonReader;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class MainActivity extends AppCompatActivity {
+    List<Accounts> accountsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AsyncTask.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    StringBuilder stringBuilder = new StringBuilder();
+                    URL api = new URL("https://igti-film.herokuapp.com/api/accounts");
+                    HttpURLConnection conn = (HttpURLConnection) api.openConnection();
+                    conn.setRequestMethod("GET");
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    retTodos(stringBuilder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    private void retTodos(StringBuilder stringBuilder) {
+        accountsList = parseJson(stringBuilder.toString());
+        for (Accounts account : accountsList) {
+            Log.i("Id", "id " + account.getId() + "\n" +
+                    "agencia " + account.getAgencia() + "\n " +
+                    "conta " + account.getConta() + "\n " +
+                    "name " + account.getName() + "\n " +
+                    "balance" + account.getBalance());
+        }
+    }
+
+    private List<Accounts> parseJson(String content) {
+        List<Accounts> accountsList = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(content);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                Accounts account = new Accounts();
+                account.setId(jsonObject.getInt("id"));
+                account.setAgencia(jsonObject.getInt("agencia"));
+                account.setConta(jsonObject.getInt("conta"));
+                account.setName(jsonObject.getString("name"));
+                account.setBalance(jsonObject.getInt("balance"));
+                accountsList.add(account);
+            }
+            return accountsList;
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
